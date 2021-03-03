9fd06e09442e5706b1c0793b05ef3c712a255f93
damla <damlatiltak@gmail.com>
2021-03-03 15:36:26 +0100

    unnecessary files deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-      </Switch>
+      <h1>hey</h1>
     </div>
   );
 }
