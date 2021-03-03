e3094a5efded09796f04848e0006d71ddf671970
Thomas de Groot <degroot12@gmail.com>
2021-03-03 15:34:47 +0100

    added signin/signout/signup

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+
+function SignIn(props) {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        Sign In
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={props.onSignIn}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control name='email' type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control name='password' type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default SignIn;
