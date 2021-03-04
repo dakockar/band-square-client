eb6e28e8e70dcfc8ab5d3f5d53abe1bea73aa4fc
damla <damlatiltak@gmail.com>
2021-03-04 10:38:19 +0100

    profile page corrected

--- src/components/MusicianProfile.jsx
+++ src/components/MusicianProfile.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { propTypes } from 'react-bootstrap/esm/Image'
-import MusicianSearch from './MusicianSearch'
 
 function MusicianProfile(props) {
     console.log('props------', props)
     return (
         <div>
 
-            <img src={props.imgUrl}/>
-            <h5>Name: {props.email}</h5>
-            <h5>Instrument: {props.instrument}</h5>
+            <img src={props.user.imgUrl} />
+            <h5>Name: {props.user.email}</h5>
+            <h5>Instrument: {props.user.instrument}</h5>
 
         </div>
     )
