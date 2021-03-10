3ae4d4a75de112188b510214364ac6a25b888195
damla <damlatiltak@gmail.com>
2021-03-10 14:52:20 +0100

    client side email and password validation done

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,12 +1,47 @@
 import React, { useState } from "react";
-import { Modal, Form, Button } from "react-bootstrap";
+// import { Modal, Form, Button, InputGroup } from "react-bootstrap";
+import { Modal, Col, Button } from "react-bootstrap";
+import { Form, FormGroup, Input, Label, FormFeedback, FormText } from "reactstrap";
 
 function SignIn(props) {
-
   const [show, setShow] = useState(false);
+  const [email, setEmailState] = useState(false);
+  // const [password, setPasswordState] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const validateEmail = (e) => {
+    // regex for email validation
+    const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let emailState = ""
+
+    if (!e.target.value.length) {
+      setEmailState(false);
+      return;
+    }
+
+    emailRegEx.test(e.target.value) ? emailState = "has-success" : emailState = "has-danger";
+
+    setEmailState(emailState);
+  }
+
+  // const validatePassword = (e) => {
+  //   const passRegEx = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  //   let passwordState = "";
+
+  //   if (e.target.value.length === 0) {
+  //     setPasswordState(false);
+  //     return;
+  //   }
+
+  //   passRegEx.test(e.target.value) ? passwordState = "has-success" : passwordState = "has-danger";
+
+
+  //   // this.setState({ passwordState });
+  //   setPasswordState(passwordState);
+  // }
+
 
   return (
     <div>
@@ -19,23 +54,47 @@ function SignIn(props) {
           <Modal.Title>Sign In</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={props.onSignIn}>
+
+          <Form onSubmit={props.onSignUp}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input onChange={validateEmail}
+                valid={email === 'has-success'}
+                invalid={email === 'has-danger'}
+                type="email" name="email" id="email" placeholder="email" />
+              <FormFeedback valid>Sweet! That email is valid.</FormFeedback>
+              <FormFeedback invalid="true">Please enter a valid email.</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                // onChange={validatePassword}
+                // valid={password === 'has-success'}
+                // invalid={password === 'has-danger'}
+                type="password" name="password" id="password" placeholder="password" />
+              {/* <FormFeedback valid>strong password!</FormFeedback>
+              <FormFeedback invalid="true">Your password is not strong enough</FormFeedback> */}
+            </FormGroup>
+            <Button variant="dark">Sign Up!</Button>
+          </Form>
+
+
+
+
+          {/* <Form onSubmit={props.onSignIn}>
             <Form.Group controlId="email">
               <Form.Label>Email address</Form.Label>
-              <Form.Control name='email' type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              <Form.Control required name='email' type="email" placeholder="Enter email" />
             </Form.Group>
 
             <Form.Group controlId="password">
               <Form.Label>Password</Form.Label>
               <Form.Control name='password' type="password" placeholder="Password" />
             </Form.Group>
-            <Button variant="dark" type="submit" onClick={handleClose}>
+            <Button variant="dark" type="submit">
               Sign In!
             </Button>
-          </Form>
+          </Form> */}
         </Modal.Body>
       </Modal>
     </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,45 @@
 import React, { useState } from "react";
-import { Modal, Form, Button, Row, Col } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
+import { Form, FormGroup, Input, Label, FormFeedback, FormText } from "reactstrap";
+
 
 function SignUp(props) {
   const [show, setShow] = useState(false);
+  const [email, setEmailState] = useState(false);
+  const [password, setPasswordState] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const validateEmail = (e) => {
+    // regex for email validation
+    const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let emailState = ""
+
+    if (!e.target.value.length) {
+      setEmailState(false);
+      return;
+    }
+
+    emailRegEx.test(e.target.value) ? emailState = "has-success" : emailState = "has-danger";
+
+    setEmailState(emailState);
+  }
+
+  const validatePassword = (e) => {
+    const passRegEx = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    let passwordState = "";
+
+    if (!e.target.value.length) {
+      setPasswordState(false);
+      return;
+    }
+
+    passRegEx.test(e.target.value) ? passwordState = "has-success" : passwordState = "has-danger";
+
+    setPasswordState(passwordState);
+  }
+
 
   return (
     <div>
@@ -18,7 +52,45 @@ function SignUp(props) {
           <Modal.Title>Sign Up</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+
           <Form onSubmit={props.onSignUp}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input onChange={validateEmail}
+                valid={email === 'has-success'}
+                invalid={email === 'has-danger'}
+                type="email" name="email" id="email" placeholder="email" />
+              <FormText>We'll never share your email with anyone else.</FormText>
+              <FormFeedback valid>Sweet! That email is valid.</FormFeedback>
+              <FormFeedback invalid="true">Please enter a valid email.</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input onChange={validatePassword}
+                valid={password === 'has-success'}
+                invalid={password === 'has-danger'}
+                type="password" name="password" id="password" placeholder="password" />
+              <FormFeedback valid>Nice! Your password is strong!</FormFeedback>
+              <FormFeedback invalid="true">Your password is not strong enough.</FormFeedback>
+              <FormText>Password must be at least 8 characters, and include at least 1 letter and 1 number.</FormText>
+            </FormGroup>
+            <FormGroup tag="fieldset">
+              <h6>Sign me up as:</h6>
+              <FormGroup check>
+                <Input id="musician" type="radio" name="type" value="musician" />
+                <Label check for="musician">Musician</Label>
+              </FormGroup>
+              <FormGroup check>
+                <Input id="owner" type="radio" name="type" value="owner" />
+                <Label check for="owner">Owner</Label>
+              </FormGroup>
+            </FormGroup>
+
+            <Button variant="dark">Sign Up!</Button>
+          </Form>
+
+
+          {/* <Form onSubmit={props.onSignUp}>
             <Form.Group controlId="email">
               <Form.Label>Email address</Form.Label>
               <Form.Control
@@ -39,8 +111,8 @@ function SignUp(props) {
                 placeholder="Password"
               />
             </Form.Group>
-            <Form.Group as={Row}>
-              <Form.Label as="legend" column sm={2}>
+            <Form.Group>
+              <Form.Label as="legend" column sm={10}>
                 <span>Sign me up as:</span>
               </Form.Label>
               <Col sm={10}>
@@ -63,7 +135,7 @@ function SignUp(props) {
             <Button variant="dark" type="submit">
               Sign Up!
             </Button>
-          </Form>
+          </Form> */}
         </Modal.Body>
       </Modal>
     </div>
