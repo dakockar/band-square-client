8222826f5acfea20263e553ad86bdd883daa656f
Thomas de Groot <degroot12@gmail.com>
2021-03-11 13:32:08 +0100

    changed server

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -10,7 +10,7 @@ import io from "socket.io-client";
 import config from '../config';
 
 let socket;
-const CONNECTION_PORT = "localhost:5005/";
+const CONNECTION_PORT = "http://band-square.herokuapp.com";
 
 function Chat(props) {
   // Before Login
